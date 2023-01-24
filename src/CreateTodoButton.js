a08f9b9172fffe3c2352290e126a63b8972152c4
Marco Alducin <sokram_r5_1992@hotmail.com>
2023-01-24 17:11:53 -0600

    Clase de manejo de eventos en React

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton() {
+  /* Evento cuando se hace clic recibiendo un argumento en la funcion */
+  const onClickButton = (msg) =>{
+    alert(msg);
+  };
+
   return (
-      <button className="CreateTodoButton">+</button>
+    /*Envolver nuestro codigo en una arrow funcion*/
+    /*() => console.log("click!")*/
+    /*Cuando se envie un argumento al evento */
+    <button className="CreateTodoButton" onClick={() => onClickButton("Aqui se deberia abrir el modal")}>+</button>
   );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert("Ya completaste el TODO " + props.text);
+  };
+  const onDelete = () => {
+    alert("Borraste el TODO " + props.text);
+  };
+
   return (
     <li className="TodoItem">
         {/* agregar condicionales en string con variables de javacript */}
-        <span className={`pendiente ${props.completed && 'correcto'}`}>√</span>
+        <span className={`pendiente ${props.completed && 'correcto'}`} onClick={onComplete}>√</span>
         <p className={`${props.completed && 'cumplido'}`}>{ props.text }</p>
-        <span className="Eliminar">X</span>
+        {/* Declarando una funcion en un evento, cuando no se le envia argumentos a la misma, no se esta llamando a la ejecucion */}
+        <span className="Eliminar" onClick={onDelete}>X</span>
     </li>
   );
 }
