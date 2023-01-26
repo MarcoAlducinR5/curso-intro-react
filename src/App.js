d66f147e3b4885b29704e3a10a2bb1df1ed6de89
Marco Alducin <sokram_r5_1992@hotmail.com>
2023-01-25 18:47:12 -0600

    Funcionalidad de completar/no completar Todo's y eliminacion de un Todo

--- src/App.js
+++ src/App.js
@@ -35,6 +35,36 @@ function App() {
     });
   }
 
+  /* funcion para marcar como completado un Todo */
+  const completeTodo = (text) => {
+    /*Encontrar el index del todo de acuerdo al texto del Todo */
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+
+    /*Clonando el arreglo declarado anteriormente*/
+    const newTodos = [...todos];
+    /*forma corta */
+    /*cambiando la propiedad de completed a true en esa posicion*/
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    /*forma larga */
+    /* newTodos[todoIndex] = {
+      text: newTodos[todoIndex].text,
+      completed: true
+    } */
+    /*Re-Renderizar el arreglo de Todos con los nuevos valores */
+    setTodos(newTodos);
+  };
+  
+  /* funcion para eliminar un Todo */
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+
+    const newTodos = [...todos];
+    /*forma corta */
+    /*usando el metodo splice enviandole que en la posicion tal (todoIndex) e indicandole que solo sera 1 a eliminar*/
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
   return (
     /*Renderizado de una sola etiqueta por componente*/
     <React.Fragment>
@@ -50,6 +80,8 @@ function App() {
             key={todo.text} 
             text={todo.text} 
             completed={todo.completed} 
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList> 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,20 +2,13 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-  const onComplete = () => {
-    alert("Ya completaste el TODO " + props.text);
-  };
-  const onDelete = () => {
-    alert("Borraste el TODO " + props.text);
-  };
-
   return (
     <li className="TodoItem">
         {/* agregar condicionales en string con variables de javacript */}
-        <span className={`pendiente ${props.completed && 'correcto'}`} onClick={onComplete}>√</span>
+        <span className={`pendiente ${props.completed && 'correcto'}`} onClick={props.onComplete}>√</span>
         <p className={`${props.completed && 'cumplido'}`}>{ props.text }</p>
         {/* Declarando una funcion en un evento, cuando no se le envia argumentos a la misma, no se esta llamando a la ejecucion */}
-        <span className="Eliminar" onClick={onDelete}>X</span>
+        <span className="Eliminar" onClick={props.onDelete}>X</span>
     </li>
   );
 }
